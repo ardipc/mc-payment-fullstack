73bce4504ada4fe973e4bda0669634b6da13e1f2
Ahmad Ardiansyah <ardiansyah3ber@gmail.com>
2021-12-14 16:04:04 +0700

    Create 1.logic.js

---
+++ 1.logic.js
@@ -0,0 +1,16 @@
+let nums = [2,7,11,15];
+let target = 13;
+
+let result = [];
+
+for(var i=0; i<nums.length; i++) {
+  if(target > nums[i]) {
+    let hasilDiKurangi = target - nums[i];
+    let cariYangCocok = nums.filter(item => item === hasilDiKurangi);
+    if(cariYangCocok.length) {
+      result.push(i)
+    }
+  }
+}
+
+console.log(result);
