69e5eb43cfb05b4f9759585a257561845e9a1b7c
Ahmad Ardiansyah <ardiansyah3ber@gmail.com>
2021-12-16 13:01:19 +0700

    add footer

---
+++ components/footer.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer className="p-3">
+      <div className="text-center">
+        <h3 className="p-3">Spending & Earning App</h3>
+        <p>Mudah, cepat, dan informatif untuk keperluan Anda melaporkan semua transaksi keuangan.</p>
+        <p>Copyright © 2021 Ahmad Ardiansyah.</p>
+      </div>
+    </footer>
+  )
+}
